0b3013364beefad90711fc4fb015bae28dbea063
GabrialStanko15 <gabrial.stanko@patelhs.org>
2021-02-09 02:08:33 +0000

    changed code

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 public class Main {
-  static int addMethod(int x) {
-    return 20 + x;
+
+  
+  static void checkAge(int age) {
+
+    
+    if (age < 18) {
+      System.out.println("Access denied - You are not old enough!");
+
+    
+    } else {
+      System.out.println("Access granted - You are old enough!");
+    }
+
   }
 
   public static void main(String[] args) {
-    System.out.println(addMethod(8));
+    checkAge(20); 
   }
 }
-// Outputs 28 (20 + 8)
